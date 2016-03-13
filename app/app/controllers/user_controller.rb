ccfd45284755195f0464168c52026c6ca4e910fd
chikashi <chikashi.ni.ym@gmail.com>
2016-03-13 10:53:39 +0000

    create user controller

---
+++ app/app/controllers/user_controller.rb
@@ -0,0 +1,8 @@
+# coding: utf-8
+class UserController < ApplicationController
+
+def index
+ render text: 'Hello, world'
+end
+
+end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -53,4 +53,12 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :user do
+    collection do
+      get :index
+    end
+  end
+
+
+
 end
